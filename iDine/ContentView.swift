24f88d4b7883ea7b2cb28104762c85086cd2f42a
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-10-07 23:51:42 +0200

    Create `ItemRow` view

---
+++ iDine/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Zeljko Lucic on 7.10.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            ItemRow(item: item)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Zeljko Lucic on 7.10.23..
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
