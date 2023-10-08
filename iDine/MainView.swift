688c91dcec01f54313f74a8a0cece1e6b50074f9
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-10-08 08:57:26 +0200

    Wrap `ContentView` and `OrderView` inside `TabView`

---
+++ iDine/MainView.swift
@@ -0,0 +1,28 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Zeljko Lucic on 8.10.23..
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .environmentObject(Order())
+}
