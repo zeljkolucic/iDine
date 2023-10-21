49e649e905f6b16105ebd9fc8476937b957eb0a1
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-10-21 11:54:54 +0200

    Use `segmented` `Picker` style to select tip amount

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -16,6 +16,9 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
+    
     var body: some View {
         Form {
             Section {
@@ -27,6 +30,19 @@ struct CheckoutView: View {
                 Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
                 if addLoyaltyDetails {
                     TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section("Total: $100") {
+                Button("Confirm Order") {
+                    // Place the order
                 }
             }
         }
